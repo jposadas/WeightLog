fca67a7bb2342b48cd3e817dd629ee2514276251
Juan Posadas <jposadas@Juan-Posadass-MacBook-Pro.local>
2013-08-08 22:22:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightLog::Application.config.secret_token = '1343092cc101c67117439eacb40d907774bf72d77f88e9e8f03dc8dc390a7c1c4a14c4784f8f3e15b44d64fefec3a884982d2a7950b2898c8e0e985d4c4075ba'
